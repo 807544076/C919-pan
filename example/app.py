c6213d9130e4a7b14ee3cbd7362a5a7b72c7ab10
oceanfish <807544076@qq.com>
2022-08-15 00:06:50 +0800

    temporary

--- example/app.py
+++ example/app.py
@@ -139,6 +139,7 @@ def logout():
     session.clear()
     return redirect(url_for('testpage_login'))
 
+
 @pages.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html')
