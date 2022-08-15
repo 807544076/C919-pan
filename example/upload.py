d10ce31fb1f09de01595fe56c0f4781ac2bdc9ff
viewtifulLeaf <viewtifulleaf@qq.com>
2022-08-15 19:36:31 +0800

    upload函数参数类型修复，其他内容小修复

--- example/upload.py
+++ example/upload.py
@@ -3,10 +3,13 @@ from hashlib import md5
 from linksql import C919SQL
 import nacl
 
+tempDir='./temp/'
 fileStorage='./fileStorage/'
 fileTypeWhiteList=['jpg','png','gif','jpeg','bmp','doc','docx','xls','xlsx','ppt','pptx']
 
 def dirCheck(email):
+    if not path.exists(tempDir):
+        mkdir(tempDir)
     if not path.exists(fileStorage):
         mkdir(fileStorage)
     db=C919SQL()
@@ -33,17 +36,17 @@ def fileExist(newFileHash):
         db.end_link()
         return False
 
-def upload(email,file):
+def upload(email,file,nameString):
     userDir=dirCheck(email)
-    fileName=path.splitext(file)[0]
-    fileExtension=path.splitext(file)[1][1:]
+    fileName=path.splitext(nameString)[0]
+    fileExtension=path.splitext(nameString)[1][1:]
     assert fileExtension in fileTypeWhiteList,'不允许的文件类型'
     assert len(fileName)<=45,'文件名过长,超过45个字符'
-    fileContent=open(file,'rb').read()
+    fileContent=file.read()
     fileSize=len(fileContent)
     assert fileSize<=1024*1024*10,'文件过大,超过10MB限制'
     fileHash=md5(fileContent).hexdigest()
-    if fileExist(fileName,fileHash):
+    if fileExist(fileHash):
         db=C919SQL()
         db.admin_link()
         userUUID=db.selectUserUUID(email)
@@ -57,5 +60,5 @@ def upload(email,file):
         db.upload_file(fileName,userUUID,fileHash,fileSize)
         db.end_link()
         fileContentEncrypted=nacl.public.SealedBox(userPasswordHash).encrypt(fileContent)
-        with open(userDir+'/'+fileName,'wb') as f:
+        with open(userDir+'/'+fileName+fileHash,'wb') as f:
             f.write(fileContentEncrypted)
