51033f039618d807ec059bfd0bae4326c5d9b5ec
oceanfish <807544076@qq.com>
2022-08-15 18:12:40 +0800

    移植了daican的forgot界面和set_password界面 完成了忘记密码和修改密码功能

--- example/app.py
+++ example/app.py
@@ -15,7 +15,7 @@ HASHER = nacl.hash.sha512
 hash_msg = bytes(str(time.time()), 'utf-8')
 digest = HASHER(hash_msg, encoder=nacl.encoding.HexEncoder)
 
-pages=Flask(__name__)
+pages = Flask(__name__)
 pages.secret_key = digest
 
 pages.config['SESSION_PERMANENT'] = True
@@ -24,8 +24,10 @@ pages.config['SESSION_TYPE'] = 'filesystem'
 Session(pages)
 
 r_info = ['', '', '', '']   # [0]用户名，[1]hash邮箱，[2]hash密码，[3]原邮箱
-check_num = []
+check_num = [0]
 start_time = [0]
+check_fun = [0]  # 0 代表注册检测，1 代表忘记密码检测
+forgot_flag = [0]   # 转跳标记位
 
 
 @pages.route('/')
@@ -53,42 +55,75 @@ def login():
         return render_template('login.html', csrf=session.get('csrf'))
 
 
-@pages.route('/forgot')
+@pages.route('/forgot', methods=['GET', 'POST'])
 def forgot():
     if request.method == 'POST':
-        email = request.form['email']
-        if email == '':
+        r_info[3] = request.form['email']
+        bytes_email = bytes(request.form['email'], 'utf-8')
+        email = HASHER(bytes_email, encoder=nacl.encoding.HexEncoder).decode('utf-8')
+        r_info[1] = email
+        if request.form['email'] == '':
             flash('请输入邮箱')
             return redirect(url_for('forgot'))
         else:
             sql = C919SQL()
+            sql.search_link()
             if sql.check_email(email):
-                flash('已发送验证邮件，请查收')
-                sendMail(email)
-                return redirect(url_for('set_password'))
+                sql.end_link()
+                check_num[0] = random.randint(100000, 999999)
+                message = """
+                    <body style="height: 100vh; display: flex; justify-content: center; align-items: center; background-color: linear-gradient(200deg, #cbdcf5, #fce7ef);">
+                        <div class="show" style="font: size 12px;">
+                            <p>您好，</p>
+                            <p>这里是 C919 中传放心传，您的验证码为：</p><br>
+                            <p style="font-size: 30px;"><strong>""" + str(check_num[0]) + """</strong></p><br>
+                            <p>本验证码有效期为 5 分钟，仅可使用一次。</p>
+                            <p>您没有收到来自 C919 的输入验证码要求，却收到了这封邮件？如果是这样，您的账号可能有安全隐患。请尽快更改您的密码。</p>
+                            <p>诚挚祝福，</p>
+                            <p>来自 C919 指挥部</p>
+                        </div>
+                    </body>
+                    """
+                sendMail(message, 'C919注册邮箱验证', 'C919指挥部', 'User', request.form['email'])
+                start_time[0] = time.time()
+                check_fun[0] = 1
+                return redirect(url_for('email_check'))
             else:
+                sql.end_link()
                 flash('邮箱未注册')
                 return redirect(url_for('forgot'))
-    return render_template('forgot.html', pageName='forgot')
+    else:
+        if session.get('name'):
+            session.clear()
+        return render_template('forgot.html')
 
 
-@pages.route('/set_password')
-def setpassword():
+@pages.route('/set_password', methods=['GET', 'POST'])
+def set_password():
     if request.method == 'POST':
-        passwd = request.form['passwd']
-        if passwd == '':
+        bytes_password = bytes(request.form['password'], 'utf-8')
+        passwd = HASHER(bytes_password, encoder=nacl.encoding.HexEncoder).decode('utf-8')
+        if request.form['password'] == '$2a$12$26adcdf68dfb176d4c876u6ocbRW0ZNqedwx5ZlKIxmPI/H/wwele':
             flash('请输入密码')
             return redirect(url_for('set_password'))
         else:
             sql = C919SQL()
-            if sql.set_password(passwd):
+            sql.admin_link()
+            if sql.reset_passwd(r_info[1], passwd):
                 flash('密码设置成功')
+                sql.end_link()
                 return redirect(url_for('login'))
             else:
                 flash('密码设置失败')
+                sql.end_link()
+                forgot_flag[0] = 1
                 return redirect(url_for('set_password'))
-        return redirect(url_for('login'))
-    return render_template('set_password.html', pageName='setpassword')
+    else:
+        if forgot_flag[0] == 1:
+            return render_template('set_password.html', user_email=r_info[3])
+        else:
+            return render_template('403.html')
+
 
 @pages.route('/testpage_register',methods=['GET', 'POST'])
 def testpage_register():
@@ -104,9 +139,9 @@ def testpage_register():
             r_info[3] = (request.form['email'])
             sql = C919SQL()
             sql.search_link()
-            if not sql.select_email(r_info[1]):
+            if not sql.check_email(r_info[1]):
                 sql.end_link()
-                check_num.append(random.randint(100000, 999999))
+                check_num[0] = random.randint(100000, 999999)
                 message = """
                                 <body style="height: 100vh; display: flex; justify-content: center; align-items: center; background-color: linear-gradient(200deg, #cbdcf5, #fce7ef);">
                                     <div class="show" style="font: size 12px;">
@@ -122,19 +157,21 @@ def testpage_register():
                                 """
                 sendMail(message, 'C919注册邮箱验证', 'C919指挥部', 'New User', request.form['email'])
                 start_time[0] = time.time()
+                check_fun[0] = 0
                 return redirect(url_for('email_check'))
             else:
                 sql.end_link()
                 flash('邮箱已注册！请重新输入')
                 return redirect(url_for('login'))
         else:
-            return redirect(url_for('403'))
+            return render_template('403.html')
     else:
         return redirect(url_for('login'))
 
 
 @pages.route('/testpage_login', methods=['GET', 'POST'])
 def testpage_login():
+    forgot_flag[0] = 0
     if session.get('name'):
         return redirect(url_for('index', userName=session.get('name')))
     if request.method == 'POST':
@@ -143,8 +180,8 @@ def testpage_login():
             bytes_password = bytes(request.form['password'], 'utf-8')
             selectSQL = C919SQL()
             selectSQL.search_link()
-            if selectSQL.select_email(HASHER(bytes_email, encoder=nacl.encoding.HexEncoder).decode('utf-8')):
-                if selectSQL.select_password(HASHER(bytes_password, encoder=nacl.encoding.HexEncoder).decode('utf-8')):
+            if selectSQL.check_email(HASHER(bytes_email, encoder=nacl.encoding.HexEncoder).decode('utf-8')):
+                if selectSQL.check_password(HASHER(bytes_password, encoder=nacl.encoding.HexEncoder).decode('utf-8')):
                     session['name']=selectSQL.select_username(HASHER(bytes_email, encoder=nacl.encoding.HexEncoder).decode('utf-8'))[0]
                     return redirect(url_for('index', userName=session.get('name')))
                 else:
@@ -154,7 +191,7 @@ def testpage_login():
                 flash('用户不存在！')
                 return redirect(url_for('login'))
         else:
-            return redirect(url_for('403'))
+            return render_template('403.html')
     else:
         return redirect(url_for('login'))
 
@@ -163,22 +200,26 @@ def testpage_login():
 def email_check():
     if request.method == 'POST':
         if time.time() - start_time[0] > 300:
-            print(time.time() - start_time[0])
+            print(start_time[0])
             flash('验证超时！请重新输入')
             return redirect(url_for('login'))
         if request.form['check'] == str(check_num[0]):
-            sql = C919SQL()
-            sql.admin_link()
-            sql.userCreate(r_info[0], r_info[1], r_info[2])
-            sql.end_link()
-            session['name'] = r_info[0]
-            return redirect(url_for('index', userName=session.get('name')))
+            if check_fun[0] == 0:
+                sql = C919SQL()
+                sql.admin_link()
+                sql.userCreate(r_info[0], r_info[1], r_info[2])
+                sql.end_link()
+                session['name'] = r_info[0]
+                return redirect(url_for('index', userName=session.get('name')))
+            else:
+                check_fun[0] = 0
+                forgot_flag[0] = 1
+                return redirect(url_for('set_password'))
         else:
             flash('验证码错误！请重新输入')
-            print(request.form['check'])
             return redirect(url_for('email_check'))
     else:
-        return render_template('403.html')
+        return render_template('email_check.html')
 
 
 @pages.route('/logout', methods=['GET', 'POST'])
